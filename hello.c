42d542c9125d987e11dc1dd2e86fe9da15bb6efe
maxb10206-eng <maxb10206@gmail.com>
2026-08-19 23:27:58 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,6 @@
 #include <compiler.h>
 #include <kpmodule.h>
 #include <linux/printk.h>
-#include <linux/types.h>
 #include <kputils.h>
 #include <hook.h>
 
@@ -15,7 +14,9 @@ KPM_AUTHOR("KC");
 KPM_DESCRIPTION("RDI CPU buffer probe");
 
 static unsigned long addr_prepare;
-static int (*get_cpu_buf)(int32_t, uintptr_t *, size_t *);
+static unsigned long addr_get_cpu_buf;
+
+static int (*get_cpu_buf)(int, unsigned long *, unsigned long *);
 
 static volatile int armed;
 static volatile unsigned int hook_count;
@@ -29,8 +30,8 @@ static void before_prepare(hook_fargs2_t *args, void *udata)
 	struct cam_packet *packet;
 	struct cam_buf_io_cfg *io_cfg;
 	unsigned int i, plane;
-	uintptr_t vaddr;
-	size_t len;
+	unsigned long vaddr;
+	unsigned long len;
 	int rc;
 
 	hook_count++;
@@ -44,8 +45,9 @@ static void before_prepare(hook_fargs2_t *args, void *udata)
 
 	packet = prepare->packet;
 
-	io_cfg = (struct cam_buf_io_cfg *)((uint8_t *)&packet->payload +
-					   packet->io_configs_offset);
+	io_cfg = (struct cam_buf_io_cfg *)(
+		(unsigned char *)&packet->payload +
+		packet->io_configs_offset);
 
 	for (i = 0; i < packet->num_io_configs; i++) {
 		if (io_cfg[i].direction != CAM_BUF_OUTPUT)
@@ -59,7 +61,7 @@ static void before_prepare(hook_fargs2_t *args, void *udata)
 
 		pr_info("cam-raw-dump: RDI res=0x%x req=%llu\n",
 			io_cfg[i].resource_type,
-			packet->header.request_id);
+			(unsigned long long)packet->header.request_id);
 
 		for (plane = 0; plane < CAM_PACKET_MAX_PLANES; plane++) {
 			if (!io_cfg[i].mem_handle[plane])
@@ -68,17 +70,22 @@ static void before_prepare(hook_fargs2_t *args, void *udata)
 			vaddr = 0;
 			len = 0;
 
-			rc = get_cpu_buf ?
-				get_cpu_buf(io_cfg[i].mem_handle[plane],
-					    &vaddr, &len) : -ENOSYS;
+			if (get_cpu_buf)
+				rc = get_cpu_buf(
+					io_cfg[i].mem_handle[plane],
+					&vaddr,
+					&len);
+			else
+				rc = -1;
 
-			pr_info("cam-raw-dump: RDI res=0x%x plane=%u "
-				"mem=0x%x rc=%d vaddr=%px len=%zu\n",
+			pr_info(
+				"cam-raw-dump: RDI res=0x%x plane=%u "
+				"mem=0x%x rc=%d vaddr=%lx len=%lu\n",
 				io_cfg[i].resource_type,
 				plane,
 				io_cfg[i].mem_handle[plane],
 				rc,
-				(void *)vaddr,
+				vaddr,
 				len);
 		}
 
@@ -95,17 +102,26 @@ static long cam_kpm_init(
 	addr_prepare =
 		kallsyms_lookup_name("cam_ife_mgr_prepare_hw_update");
 
-	get_cpu_buf =
-		(void *)kallsyms_lookup_name("cam_mem_get_cpu_buf");
+	addr_get_cpu_buf =
+		kallsyms_lookup_name("cam_mem_get_cpu_buf");
 
-	pr_info("cam-raw-dump: prepare=%lx cpu_buf=%lx\n",
-		addr_prepare, (unsigned long)get_cpu_buf);
+	pr_info(
+		"cam-raw-dump: prepare=%lx cpu_buf=%lx\n",
+		addr_prepare,
+		addr_get_cpu_buf);
 
-	if (!addr_prepare || !get_cpu_buf)
+	if (!addr_prepare || !addr_get_cpu_buf)
 		return -1;
 
-	if (hook_wrap2((void *)addr_prepare,
-		       before_prepare, NULL, NULL))
+	get_cpu_buf =
+		(int (*)(int, unsigned long *, unsigned long *))
+		(void *)addr_get_cpu_buf;
+
+	if (hook_wrap2(
+		(void *)addr_prepare,
+		before_prepare,
+		NULL,
+		NULL))
 		return -1;
 
 	pr_info("cam-raw-dump: init ok\n");
@@ -123,12 +139,18 @@ static long cam_kpm_control0(
 	if (args[0] == 'c') {
 		armed = 1;
 		rdi_count = 0;
+
 		pr_info("cam-raw-dump: armed\n");
 		compat_copy_to_user(out_msg, "armed", 6);
+
 	} else if (args[0] == 's') {
 		armed = 0;
-		pr_info("cam-raw-dump: stopped hooks=%u rdi=%u\n",
-			hook_count, rdi_count);
+
+		pr_info(
+			"cam-raw-dump: stopped hooks=%u rdi=%u\n",
+			hook_count,
+			rdi_count);
+
 		compat_copy_to_user(out_msg, "stopped", 8);
 	}
 
